202b0271d08a0e09b317221fd2ad4c9cd9eb27fa
Ming <1147566023@qq.com>
2016-09-24 15:33:44 +0800

    Initial Commit

---
+++ testDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testDemo
+//
+//  Created by lm on 16/9/24.
+//  Copyright © 2016年 Czm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testDemo
+//
+//  Created by lm on 16/9/24.
+//  Copyright © 2016年 Czm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
